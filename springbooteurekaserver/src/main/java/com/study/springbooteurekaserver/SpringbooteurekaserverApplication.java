bec074da5e0d9b207ee9352e4ad41177c837eb77
a18792721831 <45473764+a18792721831@users.noreply.github.com>
2020-02-21 20:05:37 +0800

    spring boot with eureka server base.

---
+++ springbooteurekaserver/src/main/java/com/study/springbooteurekaserver/SpringbooteurekaserverApplication.java
@@ -0,0 +1,14 @@
+package com.study.springbooteurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringbooteurekaserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbooteurekaserverApplication.class, args);
+    }
+
+}
---
+++ springbooteurekaserver/src/test/java/com/study/springbooteurekaserver/SpringbooteurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springbooteurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbooteurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
